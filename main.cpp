9092a0c0dae91255e45ade3b65d9e22a8288c0bd
melifeandmetime <104113285+melifeandmetime@users.noreply.github.com>
2022-05-05 11:22:41 +0700

    Update main.cpp
    mencoba menambah login dan data diri

--- main.cpp
+++ main.cpp
@@ -1,9 +1,70 @@
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int login(int i, char ch){
+    cout<<"====================="<<endl;
+    cout<<" LOGIN DULU MASSZEH  "<<endl;
+    cout<<"====================="<<endl;
+    for(int i=1; i<=3; i++){
+        string user ="";
+        string pass ="";
+        cout<<"username : ";cin>>user;
+        cout<<"password : ";
+            ch =_getch();
+            while(ch!=13){
+                pass.push_back(ch);
+                cout<<'*';
+                ch =_getch();
+            }
+    if(user=="aldi"&&pass=="54321"){
+        cout<<"\n\nAnda Berhasil Login\n"<<endl;
+        system("pause");
+        system("cls");
+        return 0;
+    }else{
+        cout<<"\n\nMaaf kami tidak mengenal username & password anda.\n\n";
+        system("pause");
+        system("cls");
+    }
+    }
+
+    while(i<=3);
+    cout << "Anda telah 3x memasukan Username & Password yang salah.\n";
+    cout << "Mohon maaf akun anda kami blokir untuk sementara. \n";
+    cout << "Silahkan hubungi kami melalui e-mail support@email.com, Terima Kasih..";
+    ;
+}
+
+struct dataDiri{
+    string nama;
+    string ttl;
+    string alamat;
+};
+
+
+int main (){
+
+   int i;
+   char ch;
+
+   login(i, ch);
+
+
+   cout<<"ANTRIAN SEMBAKO COVID-19"<<endl;
+   dataDiri dadir;
+   cout<<"Nama : ";
+   getline(cin, dadir.nama);
+   cout<<"TTL : ";
+   getline(cin, dadir.ttl);
+   cout<<"Alamat : ";
+   getline(cin, dadir.alamat);
+   system("cls");
+   cout<<"\n\n Nama   : \n\n"<<dadir.nama<<endl;
+   cout<<"\n\n TTL    : \n\n"<<dadir.ttl<<endl;
+   cout<<"\n\n Alamat : \n\n"<<dadir.alamat<<endl;
+
     return 0;
 }
